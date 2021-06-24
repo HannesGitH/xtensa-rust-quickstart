065655f050a0a489e81e11bed986e52779c21dbb
hannes <33062605+HannesGitH@users.noreply.github.com>
2021-06-24 21:26:33 +0200

    copied from MabezDev

--- setenv.ps1
+++ setenv.ps1
@@ -2,7 +2,7 @@
 # Use with ". .\setenv.ps1"
 
 # change this to the directory of where you built rustc for xtensa
-$env:CUSTOM_RUSTC = "G:\rust-xtensa"
+$env:CUSTOM_RUSTC = "D:\stuff\rust"
 
 $env:RUST_BACKTRACE = "1"
 $env:XARGO_RUST_SRC = "$env:CUSTOM_RUSTC\library" # or /src for an older compiler
