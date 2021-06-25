a4cdc9af4bad1d50e2dbe3cc2acd5b51f9eeec87
hannes <33062605+HannesGitH@users.noreply.github.com>
2021-06-25 12:25:18 +0200

    tried to use timer with Periodic and Countdown but didnt find implementation so gave up

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,96 @@
-//! NOTE: The following instructions are for building a custom Xtensa target, for the esp32 and esp8266
-//! see the examples/ folder. 
-//!
-//! Building for a custom target:
-//!   - fill out a `memory.x` file
-//!   - Create a build.rs
-//!   - Choose a `xtensa-lx` and `xtensa-lx-rt` cpu feature
-//! 
-//! Example `memory.x` (also see the ESP hals)
-//! ```
-//! /* 
-//! Specify main memory areas
-//!     These are generic ones, which will need to be filled in for your
-//!     device.
-//! 
-//!     The espXX-hal crates will handle this for you. Only use this if you're targeting
-//!     another xtensa target.
-//! */
-//! MEMORY
-//! {
-//!   vectors_seg ( RX )     : ORIGIN = 0x40080000, len =  1k /* SRAM0 */
-//!   iram_seg ( RX )        : ORIGIN = 0x40080400, len = 128k-0x400 /* SRAM0 */
-//!   dram_seg ( RW )        : ORIGIN = 0x3FFB0000, len = 176k
-//! }
-//! 
-//! REGION_ALIAS("ROTEXT", iram_seg); /* these alias' link up with `xtensa-lx-rt` */
-//! REGION_ALIAS("RWTEXT", iram_seg);
-//! REGION_ALIAS("RODATA", dram_seg);
-//! REGION_ALIAS("RWDATA", dram_seg);
-//! ```
-//! 
-//! Example `build.rs`
-//! ```
-//! use std::env;
-//! use std::fs::File;
-//! use std::io::Write;
-//! use std::path::PathBuf;
-//! 
-//! 
-//! 
-//! fn main() {
-//!     // Put the linker script somewhere the linker can find it
-//!     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
-//! 
-//!     File::create(out.join("memory.x"))
-//!         .unwrap()
-//!         .write_all(include_bytes!("memory.x.in"))
-//!         .unwrap();
-//! 
-//!     println!("cargo:rustc-link-search={}", out.display());
-//!     // Only re-run the build script when memory.x is changed,
-//!     // instead of when any part of the source code changes.
-//!     println!("cargo:rerun-if-changed=memory.x");
-//! }
-//! ```
-
 #![no_std]
 #![no_main]
 
-use xtensa_lx_rt as _;
-use xtensa_lx_rt::entry;
+use esp32_hal::target;
+use hal::prelude::*;
+use xtensa_lx::timer::delay;
 use panic_halt as _;
+use esp32_hal as hal;
+use ws2812_timer_delay as ws2812;
 
+use crate::target::{TIMG0, TIMG1};
+
+/// The default clock source is the onboard crystal
+/// In most cases 40mhz (but can be as low as 2mhz depending on the board)
+const CORE_HZ: u32 = 40_000_000;
+
+const WDT_WKEY_VALUE: u32 = 0x50D83AA1;
+
+
+impl CountDown for TIMG0{
+    type Time;
+    fn start<T>(&mut self, count: T)
+    where
+        T: Into<Self::Time>
+        {
+            self.
+        }
+    fn wait(&mut self) -> Result<(), !>;
+}
 
 #[entry]
 fn main() -> ! {
-    loop {}
+    let dp = target::Peripherals::take().expect("Failed to obtain Peripherals");
+
+    let mut rtccntl = dp.RTCCNTL;
+    let mut timg0 = dp.TIMG0;
+    let mut timg1 = dp.TIMG1;
+
+    // (https://github.com/espressif/openocd-esp32/blob/97ba3a6bb9eaa898d91df923bbedddfeaaaf28c9/src/target/esp32.c#L431)
+    // openocd disables the wdt's on halt
+    // we will do it manually on startup
+    disable_timg_wdts(&mut timg0, &mut timg1);
+    disable_rtc_wdt(&mut rtccntl);
+
+    let pins = dp.GPIO.split();
+    let mut led = pins.gpio2.into_push_pull_output();
+    
+    let mut led_extern = pins.gpio13.into_push_pull_output();
+
+    let mut data_pin = pins.gpio12.into_push_pull_output();
+
+    let mut ws = ws2812::Ws2812::new(embedded_hal, data_pin);
+
+    loop {
+        led_extern.set_high().unwrap();
+        led.set_low().unwrap();
+        delay(CORE_HZ);
+        led_extern.set_low().unwrap();
+        led.set_high().unwrap();
+        delay(CORE_HZ);
+    }
+}
+
+fn disable_rtc_wdt(rtccntl: &mut target::RTCCNTL) {
+    /* Disables the RTCWDT */
+    rtccntl
+        .wdtwprotect
+        .write(|w| unsafe { w.bits(WDT_WKEY_VALUE) });
+    rtccntl.wdtconfig0.modify(|_, w| unsafe {
+        w.wdt_stg0()
+            .bits(0x0)
+            .wdt_stg1()
+            .bits(0x0)
+            .wdt_stg2()
+            .bits(0x0)
+            .wdt_stg3()
+            .bits(0x0)
+            .wdt_flashboot_mod_en()
+            .clear_bit()
+            .wdt_en()
+            .clear_bit()
+    });
+    rtccntl.wdtwprotect.write(|w| unsafe { w.bits(0x0) });
+}
+
+fn disable_timg_wdts(timg0: &mut target::TIMG0, timg1: &mut target::TIMG1) {
+    timg0
+        .wdtwprotect
+        .write(|w| unsafe { w.bits(WDT_WKEY_VALUE) });
+    timg1
+        .wdtwprotect
+        .write(|w| unsafe { w.bits(WDT_WKEY_VALUE) });
+
+    timg0.wdtconfig0.write(|w| unsafe { w.bits(0x0) });
+    timg1.wdtconfig0.write(|w| unsafe { w.bits(0x0) });
 }
